8194fe4e8b57a4a60d02df14d58a2119e9847724
Guillermo García Subirana <guillermogarsub@gmail.com>
2019-09-27 08:06:01 +0200

    first push

---
+++ Threads/Main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdio.h>
+#include <Windows.h>
+#include <thread>
+#include <mutex>
+using namespace std;
+
+int counter = 0; // Global var
+mutex mtx; // Mutex object
+
+void increment(int iterations) {
+	for (int i = 0; i < iterations; ++i) {
+		unique_lock<mutex> lock(mtx); // Lock the mutex
+		counter++;
+	}
+}
+int main() {
+	std::thread t1(increment, 100000); // Execute 'increment' in a thread
+	std::thread t2(increment, 100000); // Execute 'increment' in a thread
+	t1.join(); // Wait for t1
+	t2.join(); // Wait for t2
+	std::cout << "Counter = " << counter << std::endl;
+
+	system("pause");
+	return 0;
+}
+
